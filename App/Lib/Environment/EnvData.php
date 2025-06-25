ec680528414572162008de926fc1c49add18b182
Daniel Filipe <72364158+Daniel10013@users.noreply.github.com>
2025-06-25 16:05:28 -0300

    Update EnvData.php

--- App/Lib/Environment/EnvData.php
+++ App/Lib/Environment/EnvData.php
@@ -5,10 +5,20 @@ namespace App\Lib\Environment;
 class EnvData
 {
     static function get(string $dataKey): string {
-        $envData = parse_ini_file('.env');
-        if(key_exists($dataKey, $envData)){
-            return $envData[$dataKey];
+        $envFile = __DIR__ . '../../../.env';
+
+        if (file_exists($envFile)) {
+            $envData = parse_ini_file($envFile);
+            if (array_key_exists($dataKey, $envData)) {
+                return $envData[$dataKey];
+            }
         }
+
+        $envValue = getenv($dataKey);
+        if ($envValue !== false) {
+            return $envValue;
+        }
+
         return '';
     }
 }
