bf6d7bd827b2f2ede985b09b6648b969c935375a
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-06-25 21:25:29 -0300

    correcao

--- App/Model/AdsModel.php
+++ App/Model/AdsModel.php
@@ -35,7 +35,6 @@ class AdsModel extends Model{
 
     public function getById($id): array {
         $query = "select * from ads_view where ad_id = :ad_id";
-        dd($query, ["ad_id" => $id]);
         return $this->select($query, ["ad_id" => $id]);
     }
 
