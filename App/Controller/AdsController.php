6bb8bc71ab365f28d510584323fb1f0703acc214
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-06-25 21:10:33 -0300

    debug render

--- App/Controller/AdsController.php
+++ App/Controller/AdsController.php
@@ -76,12 +76,8 @@ class AdsController extends Controller
     public function details(): void
     {
         $id = Url::segment(2);
-        echo "<pre>";
-        var_dump(Url::segment(0));
-        var_dump(Url::segment(1));
-        var_dump(Url::segment(2));
-        var_dump(Url::segment(3));
-        die();
+        var_dump(Url::segment(1), is_integer(Url::segment(1)), 1);
+        var_dump(Url::segment(2), is_integer(Url::segment(2)), 2);
         if(empty($id)){
             Response::badRequest('Dados invalidos para a busca');
         }
